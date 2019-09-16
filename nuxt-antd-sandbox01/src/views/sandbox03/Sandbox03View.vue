ba8e310388ffe74a4958e69e0b582ffc3a7a4a13
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2019-09-16 18:28:13 +0900

    input試し中...

--- nuxt-antd-sandbox01/src/views/sandbox03/Sandbox03View.vue
+++ nuxt-antd-sandbox01/src/views/sandbox03/Sandbox03View.vue
@@ -1,11 +1,61 @@
 <template>
   <div>
-    sandbox03
+    <a-input placeholder="xxx-input" />a-input-group
+    <a-input-group>
+      <a-col :span="2">
+        <a-input placeholder="4" />
+      </a-col>
+      <a-col :span="2">
+        <a-input placeholder="3" />
+      </a-col>
+      <a-col :span="2">
+        <a-input-number :min="1" :max="10" placeholder="2" />
+      </a-col>
+      <a-col :span="4">
+        <a-checkbox>Checkbox(ずれるね</a-checkbox>
+      </a-col>
+      <a-col :span="2">
+        <a-switch v-model="checked" checkedChildren="on" unCheckedChildren="off" />xx
+      </a-col>
+      <a-col :span="2">
+        <a-date-picker />
+      </a-col>
+    </a-input-group>a-row,a-col
+    <a-row type="flex" justify="start" align="middle">
+      <a-col :span="2">
+        <a-input placeholder="4" />
+      </a-col>
+      <a-col :span="2">
+        <a-input placeholder="3" />
+      </a-col>
+      <a-col :span="2">
+        <a-input-number :min="1" :max="10" placeholder="2" />
+      </a-col>
+      <a-col :span="4">
+        <a-checkbox>Checkbox</a-checkbox>
+      </a-col>
+      <a-col :span="2">
+        <a-switch v-model="checked" checkedChildren="on" unCheckedChildren="off" />yy
+      </a-col>
+      <a-col :span="2">
+        <a-date-picker />
+      </a-col>
+    </a-row>
+
+    <a-row type="flex" justify="start" align="middle">
+      <a-col :span="4">
+        <label>xx</label>
+        <a-input placeholder="4" />
+      </a-col>
+    </a-row>
+
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data: () => ({
+    checked: false,
+  }),
+};
 </script>
