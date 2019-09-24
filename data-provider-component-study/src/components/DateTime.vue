ded7a0b46c483337bbb4337d1d862d3dc8de74d7
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2019-09-24 23:39:30 +0900

    まずはslot

---
+++ data-provider-component-study/src/components/DateTime.vue
@@ -0,0 +1,34 @@
+<template>
+  <span>
+    <slot :dateTime="dateTime"></slot>
+  </span>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class DateTime extends Vue {
+  dateTime: Date;
+
+  id: number | null;
+
+  constructor() {
+    super();
+    this.dateTime = new Date();
+    this.id = null;
+  }
+
+  mounted() {
+    this.id = setInterval(() => {
+      this.dateTime = new Date();
+    }, 500);
+  }
+
+  beforeDestroy() {
+    if (this.id != null) {
+      clearInterval(this.id);
+    }
+  }
+}
+</script>
---
+++ data-provider-component-study/src/pages/sandbox01/Sandbox01.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <DateTime v-slot="{dateTime}">{{dateTime.toLocaleString()}}</DateTime>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import DateTime from "@/components/DateTime.vue";
+@Component({
+  components: {
+    DateTime
+  }
+})
+export default class Sandbox01 extends Vue {}
+</script>
