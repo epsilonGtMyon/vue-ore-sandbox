46aa533466aa5894a560bd55a3f100e30f9cf1c1
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2019-09-29 13:00:10 +0900

    cssの追加もれてた

--- multiselect-sandbox/src/plugins/multiselect/index.ts
+++ multiselect-sandbox/src/plugins/multiselect/index.ts
@@ -1,5 +1,6 @@
-import Vue from 'vue';
-import Multiselect from 'vue-multiselect';
+import Vue from 'vue'
+import Multiselect from 'vue-multiselect'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
 
 // register globally
-Vue.component('multiselect', Multiselect);
+Vue.component('multiselect', Multiselect)
