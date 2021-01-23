19a2627dddcaeb61c1fad7e532c2ca4bda1cb0fa
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-01-23 18:31:22 +0900

    コメント

--- directive-sandbox/src/views/sandbox02/titleDirective.ts
+++ directive-sandbox/src/views/sandbox02/titleDirective.ts
@@ -2,6 +2,7 @@ import { DirectiveOptions } from "vue";
 
 //el, binding, vnode, oldVnode
 const titleDirective: DirectiveOptions = {
+  //直接htmlの属性を書き換えてみる
   inserted(el, bindings) {
     const val = bindings.value;
     el.setAttribute("title", val);
