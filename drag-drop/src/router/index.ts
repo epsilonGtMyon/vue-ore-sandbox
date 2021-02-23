0682bf95a1203ea53a4865492ebea2719e0680d8
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-02-23 21:42:11 +0900

    add new project

---
+++ drag-drop/src/router/index.ts
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/home/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/sandbox01',
+    name: 'Sandbox01',
+    component: () => import(/* webpackChunkName: "sandbox01" */ '../views/sandbox01/Sandbox01.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ drag-drop/src/views/sandbox01/Sandbox01.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sandbox01"></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Sandbox01",
+});
+</script>
+
+<style lang="scss" scoped>
+.sandbox01 {
+  height: 100%;
+}
+</style>
